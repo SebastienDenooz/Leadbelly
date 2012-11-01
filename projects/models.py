502cf440e8a7fbf5f941968f7703564fa3728740
Sébastien Denooz <sebastien.denooz@lattirail.net>
2012-11-02 00:50:46 +0100

    Add currency proposition

--- projects/models.py
+++ projects/models.py
@@ -3,11 +3,17 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 class Project(models.Model):
+    CURRENCY_CHOICES = (
+        ('EUR', '€'),
+        ('DOL', '$'),
+        ('LIR', '£'),
+        ('YEN', '¥')
+    )
     title = models.CharField(_(u'Task name'), max_length=255)
     create_date = models.DateTimeField(auto_now=False, auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True, auto_now_add=True)
-    default_rate = models.IntegerField(_(u'Project rate'))
-    default_currency = models.CharField(_(u'Currency'), max_length=5)
+    default_unit_rate = models.IntegerField(_(u'Default unit rate'))
+    default_currency = models.CharField(_(u'Currency'), max_length=5, choices=CURRENCY_CHOICES)
 
     def __unicode__(self):
         return u'%s' % (self.title)
