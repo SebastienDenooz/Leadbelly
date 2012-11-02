7b5b45338cf7ed9f0a1fd61fc62bdb417a78a957
Sébastien Denooz <sde@voxteneo.com>
2012-11-02 07:31:38 +0100

    Add status field with possible choice

--- projects/models.py
+++ projects/models.py
@@ -2,17 +2,26 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-class Project(models.Model):
+
     CURRENCY_CHOICES = (
         ('EUR', '€'),
         ('DOL', '$'),
         ('LIR', '£'),
         ('YEN', '¥')
     )
+    PROJECT_STATE = (
+        ('0', _(u'Draft')),
+        ('1', _(u'Quotation')),
+        ('2', _(u'Bill')),
+        ('3', _(u'Refused'))
+    )
+
+class Project(models.Model):
     title = models.CharField(_(u'Task name'), max_length=255)
     create_date = models.DateTimeField(auto_now=False, auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True, auto_now_add=True)
     default_unit_rate = models.IntegerField(_(u'Default unit rate'))
+    state = models.IntegerField(_(u'Project status'), choices=PROJECT_STATE)
     default_currency = models.CharField(_(u'Currency'), max_length=5, choices=CURRENCY_CHOICES)
 
     def __unicode__(self):
