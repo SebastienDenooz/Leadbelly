c4c085552703fa9f67a5af8395d2fcf8f7b9e874
Sébastien Denooz <sde@voxteneo.com>
2012-11-02 11:21:48 +0100

    Go for tastypiegit add Leadbelly/settings.py :D

--- Leadbelly/settings.py
+++ Leadbelly/settings.py
@@ -120,6 +120,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'projects',
+    'tastypie',
 )
 
 # A sample logging configuration. The only tangible logging
