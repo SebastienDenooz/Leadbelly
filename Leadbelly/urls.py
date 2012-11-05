dbba7b55a2010ae5b4c8136a115e940465fb1754
Sébastien Denooz <sde@voxteneo.com>
2012-11-05 09:56:27 +0100

    A little play with tastypie :-)

--- Leadbelly/urls.py
+++ Leadbelly/urls.py
@@ -4,7 +4,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from projects.views import ProjectsView
+from tastypie.api import Api
+from projects.api.resources import UserResource
+
+v1_api = Api()
+v1_api.register(UserResource())
+
+from projects.views import *
 
 urlpatterns = patterns('',
     # Examples:
@@ -13,4 +19,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', ProjectsView.as_view(), name='projects-list'),
+    url(r'^add/$', ProjectCreateView.as_view(), name='project-create'),
+    url(r'^api/', include(v1_api.urls)),
 )
+
--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,21 @@
 # -*- coding: utf-8 -*-
 
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.core.urlresolvers import reverse_lazy
+from django.http import HttpResponseRedirect
 from projects.models import Task
 from projects.models import Project
+from projects.forms import ProjectForm
+
 class ProjectsView(ListView):
 
     template_name="projects/projects-list.html"
     model = Project
+
+class ProjectCreateView(CreateView):
+    form_class = ProjectForm
+    success_url  = reverse_lazy('projects-list')
+
+    def form_invalid(self, form):
+        # Attention les erreurs du form ne seront pas affichées
+        return HttpResponseRedirect(self.success_url)
